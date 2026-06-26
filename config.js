d1ac6d4fe2ac95eedc424608d4da3c6077482c6e
☠️BEOWULF☠️ <juniormegoh56@gmail.com>
2026-06-26 09:06:04 +0100

    Add configuration file for bot settings
    Initial configuration for the bot with basic settings.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  botName: '☠️BEOWULF☠️⎯꯭̽',
+  botNameMD: '☠️BEOWULF☠️⎯꯭̽-MD',
+  ownerNumber: '2348030559356',
+  prefix: '.',
+  sessionName: 'beowulf_session',
+  ownerName: 'BEOWULF',
+  version: '1.0.0'
+};
